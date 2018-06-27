da1fb72fb94ef59c1be17bbafcd7a9223817d573
Randall Degges <r@rdegges.com>
2018-06-26 19:12:04 -0700

    Adding our main app file.

---
+++ app.js
@@ -0,0 +1,83 @@
+const createError = require("http-errors");
+const express = require("express");
+const logger = require("morgan");
+const path = require("path");
+const session = require("express-session");
+const ExpressOIDC = require("@okta/oidc-middleware").ExpressOIDC;
+
+const auth = require("./auth");
+const blogRouter = require("./routes/blog");
+const usersRouter = require("./routes/users");
+
+
+// App initialization
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+// Middleware
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+const oidc = new ExpressOIDC({
+  issuer: process.env.OKTA_ORG_URL + "/oauth2/default",
+  client_id: process.env.OKTA_CLIENT_ID,
+  client_secret: process.env.OKTA_CLIENT_SECRET,
+  redirect_uri: "http://localhost:3000/users/callback",
+  scope: "openid profile",
+  routes: {
+    login: {
+      path: "/users/login"
+    },
+    callback: {
+      path: "/users/callback",
+      defaultRedirect: "/dashboard"
+    }
+  }
+});
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: true,
+  saveUninitialized: false
+}));
+
+app.use(oidc.router);
+
+app.use((req, res, next) => {
+  if (!req.userinfo) {
+    return next();
+  }
+
+  auth.client.getUser(req.userinfo.sub)
+    .then(user => {
+      req.user = user;
+      res.locals.user = user;
+
+      console.log(req.user);
+      next();
+    });
+});
+
+// Routes
+app.use("/", blogRouter);
+app.use("/users", usersRouter);
+
+// Error handlers
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+
+module.exports = app;
